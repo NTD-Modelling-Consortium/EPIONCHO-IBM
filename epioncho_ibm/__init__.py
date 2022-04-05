04a1fe28a811a35b8704014f822645f23b8ab07b
Elliott Hughes <elliott.huge@gmail.com>
2022-04-05 13:14:09 +0100

    Added several fixed params to params with comments
    * ABR
    * m
    * nuone
    * nutwo
    * mu.v
    * a.v

--- epioncho_ibm/__init__.py
+++ epioncho_ibm/__init__.py
@@ -46,6 +46,26 @@ class Params(BaseModel):
     treatment_start_iter: int  # The iteration upon which treatment commences (treat.start in R code)
     # See line 476 R code
 
+    # So-called Hard coded params
+    bite_rate_per_person_per_year = 1000  # Annual biting
+    bite_rate_per_fly_on_human = bite_rate_per_person_per_year * (
+        (1 / 104) / 0.63
+    )  # calculated as the product of the proportion of blackfly bites taken on humans 'm = ABR * [...]'
+
+    # Params within blackfly vector...
+    l1_l2_per_person_per_year = (
+        201.6189  # Per capita development rate of larvae from stage L1 to L2 'nuone'
+    )
+    l2_l3_per_person_per_year = (
+        207.7384  # Per capita development rate of larvae from stage L2 to L3 'nutwo'
+    )
+    blackfly_mort_per_person_per_year = (
+        26  # Per capita mortality rate of blackfly vectors 'mu.v'
+    )
+    blackfly_mort_from_mf_per_person_per_year = (
+        0.39  # Per capita microfilaria-induced mortality of blackfly vectors 'a.v'
+    )
+
 
 def advance_state(state: State, params: Params, n_iters: int = 1) -> State:
     def _next(state: State) -> State:
