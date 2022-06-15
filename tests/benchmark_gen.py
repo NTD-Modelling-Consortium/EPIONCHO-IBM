ebade586d68d677ee5e29fa6e76157ac890d3a5a
mark-todd <markpeter.todd@hotmail.co.uk>
2022-06-15 14:48:32 +0100

    Added benchmark generation facility

--- tests/benchmark_gen.py
+++ tests/benchmark_gen.py
@@ -7,24 +7,17 @@ from typing import Any, Dict, List, Tuple, Union
 
 import numpy as np
 from numpy.typing import NDArray
-from pydantic import BaseModel
 
 from epioncho_ibm import Params, RandomConfig, State, run_simulation
-from epioncho_ibm.state import NumericArrayStat, PeopleStats
-from tests.definitions.benchmark_data_types import OutputData, TestData
+from epioncho_ibm.state import PeopleStats
+from tests.definitions.benchmark_data_types import (
+    BenchmarkArray,
+    NTDSettings,
+    OutputData,
+    TestData,
+)
 from tests.definitions.pytest_config import PytestConfig
 from tests.definitions.utils import FlatDict, flatten_dict
-
-
-class NTDSettings(BaseModel):
-    min_year: float
-    max_year: float
-    year_steps: int
-    min_pop: int
-    max_pop: int
-    pop_steps: int
-    max_pop_years: float
-    benchmark_iters: int = 1
 
 
 def get_test_pairs(settings: NTDSettings) -> Tuple[List[Tuple[float, int]], float]:
@@ -65,7 +58,7 @@ def run_stochastic_test(end_time: float, params: Params) -> PeopleStats:
 
 def compute_mean_and_st_dev_of_pydantic(
     input_stats: List[PeopleStats],
-) -> Dict[str, NumericArrayStat]:
+) -> Dict[str, BenchmarkArray]:
     flat_dicts: List[FlatDict] = [
         flatten_dict(input_stat.dict()) for input_stat in input_stats
     ]
@@ -79,12 +72,95 @@ def compute_mean_and_st_dev_of_pydantic(
     final_dict_of_arrays: Dict[str, NDArray[np.float_]] = {
         k: np.array(v) for k, v in dict_of_arrays.items()
     }
-    return {k: NumericArrayStat.from_array(v) for k, v in final_dict_of_arrays.items()}
+    return {k: BenchmarkArray.from_array(v) for k, v in final_dict_of_arrays.items()}
+
+
+def generate_settings_file_and_model(
+    settings_folder: Path, settings_path: Path
+) -> NTDSettings:
+    while True:
+        answer = input(
+            "Settings path does not exist - would you like to make a settings file? (y/n): "
+        )
+        if answer == "y":
+            break
+        elif answer == "n":
+            raise ValueError("Settings file not found or created")
+        else:
+            continue
+
+    while True:
+        constraints = input(
+            "Please enter your end year constraints, comma separated (min_year, max_year, year_steps): "
+        )
+        items = constraints.split(",")
+        if len(items) == 3:
+            try:
+                min_year = float(items[0])
+                max_year = float(items[1])
+                year_steps = int(items[2])
+                if max_year < min_year:
+                    continue
+                break
+            except ValueError:
+                continue
+        continue
+
+    while True:
+        constraints = input(
+            "Please enter population constraints, comma separated (min_pop, max_pop, pop_steps): "
+        )
+        items = constraints.split(",")
+        if len(items) == 3:
+            try:
+                min_pop = int(items[0])
+                max_pop = int(items[1])
+                pop_steps = int(items[2])
+                if max_pop < min_pop:
+                    continue
+                break
+            except ValueError:
+                continue
+        continue
+
+    while True:
+        pop_years_string = input("Please enter the max population-years: ")
+        try:
+            pop_years = float(pop_years_string)
+            break
+        except ValueError:
+            continue
+
+    while True:
+        bench_iters_string = input("Please enter the number of benchmark iterations: ")
+        try:
+            bench_iters = int(bench_iters_string)
+            break
+        except ValueError:
+            continue
+    model = NTDSettings(
+        min_year=min_year,
+        max_year=max_year,
+        year_steps=year_steps,
+        min_pop=min_pop,
+        max_pop=max_pop,
+        pop_steps=pop_steps,
+        max_pop_years=pop_years,
+        benchmark_iters=bench_iters,
+    )
+    settings_folder.mkdir(parents=True)
+    settings_file = open(settings_path, "w+")
+    json.dump(model.dict(), settings_file, indent=2)
+    return model
 
 
 pytest_config = PytestConfig.parse_file("pytest_config.json")
-settings_path = Path(str(Path(pytest_config.benchmark_path)) + os.sep + "settings.json")
-settings_model = NTDSettings.parse_file(settings_path)
+settings_folder = Path(pytest_config.benchmark_path)
+settings_path = Path(str(settings_folder) + os.sep + "settings.json")
+if not settings_path.exists():
+    settings_model = generate_settings_file_and_model(settings_folder, settings_path)
+else:
+    settings_model = NTDSettings.parse_file(settings_path)
 
 test_pairs, total_pop_years = get_test_pairs(settings_model)
 est_base_time = 0.46
@@ -109,6 +185,6 @@ for end_year, population in test_pairs:
 end = time.time()
 print(f"Benchmark calculated in: {end-start}")
 test_data = TestData(tests=tests)
-benchmark_file_path = Path("benchmark.json")
+benchmark_file_path = Path(str(settings_folder) + os.sep + "benchmark.json")
 benchmark_file = open(benchmark_file_path, "w+")
 json.dump(test_data.dict(), benchmark_file, indent=2)
--- tests/definitions/benchmark_data_types.py
+++ tests/definitions/benchmark_data_types.py
@@ -1,15 +1,36 @@
-from typing import Dict, List
+from typing import Dict, List, Union
 
+import numpy as np
+from numpy.typing import NDArray
 from pydantic import BaseModel
 
 from epioncho_ibm import Params
-from epioncho_ibm.state import NumericArrayStat
+
+
+class BenchmarkArray(BaseModel):
+    mean: float
+    st_dev: float
+
+    @classmethod
+    def from_array(cls, array: Union[NDArray[np.float_], NDArray[np.int_]]):
+        return cls(mean=np.mean(array), st_dev=np.std(array))
+
+
+class NTDSettings(BaseModel):
+    min_year: float
+    max_year: float
+    year_steps: int
+    min_pop: int
+    max_pop: int
+    pop_steps: int
+    max_pop_years: float
+    benchmark_iters: int = 1
 
 
 class OutputData(BaseModel):
     end_year: float
     params: Params
-    people: Dict[str, NumericArrayStat]
+    people: Dict[str, BenchmarkArray]
 
 
 class TestData(BaseModel):
