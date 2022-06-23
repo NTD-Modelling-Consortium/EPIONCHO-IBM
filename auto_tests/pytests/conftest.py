aecf644313ef37bfb353f102b8d4cc61b0eb5ce9
mark-todd <markpeter.todd@hotmail.co.uk>
2022-06-23 11:30:15 +0100

    Added tests ids to auto tests

--- auto_tests/pytests/conftest.py
+++ auto_tests/pytests/conftest.py
@@ -20,12 +20,21 @@ test_data_model = autobenchmarker.test_model
 benchmark_file: test_data_model = test_data_model.parse_file(benchmark_file_path)
 
 
+def get_string_id(output_data: BaseOutputData) -> str:
+    string_out = ""
+    for p in autobenchmarker.parameters:
+        string_part = str(p) + ": " + str(getattr(output_data, p))
+        string_out += string_part + " "
+    return string_out
+
+
 @pytest.fixture(
     params=benchmark_file.tests,  # type:ignore
-    # ids=[
-    #    f"year: {str(i.end_year)} pop: {str(i.params.human_population)}"
-    #    for i in benchmark_file.tests
-    # ],
+    ids=[
+        get_string_id(i)
+        # f"year: {str(i.end_year)} pop: {str(i.params.human_population)}"
+        for i in benchmark_file.tests  # type:ignore
+    ],
 )
 async def benchmark_data(request) -> BaseOutputData:
     return request.param
