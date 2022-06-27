02489f7c736205e1964d674050fb1323b6b0584a
mark-todd <markpeter.todd@hotmail.co.uk>
2022-06-27 17:15:24 +0100

    Fixed autobenchmark config

--- auto_tests/auto_benchmarker_config.py
+++ auto_tests/auto_benchmarker_config.py
@@ -4,7 +4,7 @@ from epioncho_ibm import HumanParams, Params, State, StateStats, TreatmentParams
 
 def benchmarker_test_func_no_treat(end_time: float, population: int) -> StateStats:
     params = Params()
-    state = State(params=params, n_people=population)
+    state = State(params=params, n_people=int(population))
     state.dist_population_age(num_iter=15000)
     state.run_simulation(start_time=0, end_time=end_time)
     stats = state.to_stats()
@@ -15,7 +15,7 @@ def benchmarker_test_func_treat(end_time: float, population: int) -> StateStats:
     params = Params(
         treatment=TreatmentParams(start_time=0, interval_years=0.01),
     )
-    state = State(params=params, n_people=population)
+    state = State(params=params, n_people=int(population))
     state.dist_population_age(num_iter=15000)
     state.run_simulation(start_time=0, end_time=end_time)
     stats = state.to_stats()
