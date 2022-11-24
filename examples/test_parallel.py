c5390de9abdf8f2a73968ea8369919f972216f6b
mark-todd <markpeter.todd@hotmail.co.uk>
2022-11-24 12:18:46 +0000

    Added test parallel

---
+++ examples/test_parallel.py
@@ -0,0 +1,17 @@
+import numpy as np
+from epioncho_ibm import Params, State, TreatmentParams
+from multiprocessing import cpu_count
+from tqdm.contrib.concurrent import process_map
+
+benchmark_iters = 400
+
+def run_sim(i):
+    params = Params(treatment=TreatmentParams(start_time=0.1, interval_years=0.1))
+    state = State(params=params, n_people=400)
+    state.run_simulation(start_time=0, end_time=1, verbose=False)
+    return state.mf_prevalence_in_population()
+
+list_of_stats: list[float] = process_map(run_sim, range(benchmark_iters), max_workers=cpu_count())
+
+print(np.mean(list_of_stats))
+print(np.std(list_of_stats))
