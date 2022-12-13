003c6382b3a801a809c90c444052f85438718125
mark-todd <markpeter.todd@hotmail.co.uk>
2022-12-13 11:52:37 +0000

    Added comparison test

---
+++ examples/test_compare.py
@@ -0,0 +1,9 @@
+from epioncho_ibm import (
+    Params,
+    TreatmentParams,
+    make_state_from_params,
+)
+params = Params(treatment=TreatmentParams(start_time=3))
+
+state = make_state_from_params(params=params, n_people=400)
+state.run_simulation(start_time=0, end_time=5)
