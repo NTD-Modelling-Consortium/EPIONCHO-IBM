2ee91c343e4b333e6eeb4e2a408a3d03b3498281
mark-todd <markpeter.todd@hotmail.co.uk>
2022-12-13 15:29:43 +0000

    Updated test new to 400

--- examples/test_new.py
+++ examples/test_new.py
@@ -13,7 +13,7 @@ np.random.seed(0)
 
 params = Params(treatment=TreatmentParams(start_time=3))
 
-state = make_state_from_params(params=params, n_people=10)
+state = make_state_from_params(params=params, n_people=400)
 state.run_simulation(start_time=0, end_time=5, verbose=True)
 print(state._people)
 print(state.mf_prevalence_in_population())
