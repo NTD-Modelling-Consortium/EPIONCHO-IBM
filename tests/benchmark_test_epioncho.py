fee8196c2cc584debf9ac577ca1ea32e4149f60e
Marcin Krupowicz <ma@rcin.me>
2023-01-05 16:23:04 +0000

    Removed calc_failure_prob as it is moved to pytest_trust_random

--- tests/benchmark_test_epioncho.py
+++ tests/benchmark_test_epioncho.py
@@ -1,4 +1,4 @@
-from pytest_trust_random import TrustRandomConfig, benchmark_test
+from pytest_trust_random import TrustRandomConfig, benchmark_test, calc_failure_prob
 
 from epioncho_ibm import Params, StateStats, TreatmentParams
 from epioncho_ibm.state import make_state_from_params
@@ -6,7 +6,7 @@ from epioncho_ibm.state import make_state_from_params
 pytest_config = TrustRandomConfig(
     acceptable_st_devs=2.5,
     re_runs=5,
-    benchmark_path=f"benchmark",
+    benchmark_path="benchmark",
 )
 
 
@@ -26,3 +26,7 @@ def treatment(end_time: float, population: int) -> StateStats:
     state = make_state_from_params(params=params, n_people=int(population))
     state.run_simulation(start_time=0, end_time=end_time)
     return state.to_stats()
+
+
+if __name__ == "__main__":
+    calc_failure_prob(pytest_config)
