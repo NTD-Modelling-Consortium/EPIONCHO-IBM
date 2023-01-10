edc53894a7b832cd84b36a196646a7a7d7feee19
Marcin Krupowicz <ma@rcin.me>
2023-01-10 09:59:08 +0000

    Docs

--- epioncho_ibm/simulation.py
+++ epioncho_ibm/simulation.py
@@ -37,6 +37,16 @@ class Simulation:
         verbose: bool = False,
         debug: bool = False,
     ) -> None:
+        """Create a new simulation, given the parameters.
+
+        Args:
+            start_time (float): start time of the simulation
+            params (Params): simulation's parameters
+            n_people (int): number of people in the simulation
+            gamma_distribution (float, optional): Gamma distribution. Defaults to 0.3.
+            verbose (bool, optional): Verbose?. Defaults to False.
+            debug (bool, optional): Debug?. Defaults to False.
+        """
         ...
 
     @overload
@@ -47,6 +57,13 @@ class Simulation:
         verbose: bool = False,
         debug: bool = False,
     ) -> None:
+        """Restore the simulation from a previously saved file.
+
+        Args:
+            input (FileType): input file/stream
+            verbose (bool, optional): Verbose?. Defaults to False.
+            debug (bool, optional): Debug?. Defaults to False.
+        """
         ...
 
     def __init__(
@@ -98,6 +115,11 @@ class Simulation:
         return self._derived_params
 
     def reset_parameters(self, params: Params):
+        """Reset the parameters
+
+        Args:
+            params (Params): New set of parameters
+        """
         self.params = params
 
     def _advance(self):
@@ -226,21 +248,73 @@ class Simulation:
 
         self.state.current_time += self.params.delta_time
 
-    def save(self, output: FileType):
+    def save(self, output: FileType) -> None:
+        """Save the simulation to a file/stream.
+
+        The output file will be in a HDF5 format. The simulation can then be
+        restored with `Simulation.restore` class method.
+
+        Args:
+            output (FileType): output file/stream
+        """
         self.state.to_hdf5(output)
 
     @classmethod
-    def restore(cls, input: FileType):
+    def restore(cls, input: FileType) -> "Simulation":
+        """Restore the simulation from a file/stream
+
+        Args:
+            input (FileType): HDF5 stream/file
+
+        Returns:
+            Simulation: restored simulation
+        """
         return Simulation(input=input)
 
     @overload
     def iter_run(self, *, end_time: float, sampling_interval: float) -> Iterator[State]:
+        """Run the simulation until `end_time`. Generates stats every `sampling_interval`,
+        until `end_time`.
+
+        This is a generator, so you must it as one.
+
+        Examples:
+            >>> simulation = Simulation(start_time=0, params=Params(), n_people=400)
+            >>> list(simulation.iter_run(end_time=3, sampling_interval=1.0))
+            [0.99, 0.6, 0.2]
+
+        Args:
+            end_time (float): end time
+            sampling_interval (float): State sampling interval (years)
+
+        Yields:
+            Iterator[State]: Iterator of the simulation's state.
+        """
         ...
 
     @overload
     def iter_run(
         self, *, end_time: float, sampling_years: list[float]
     ) -> Iterator[State]:
+        """Run the simulation until `end_time`. Generates stats for every year in `sampling_years`.
+
+        This is a generator, so you must it as one.
+
+        Examples:
+            >>> simulation = Simulation(start_time=0, params=Params(), n_people=400)
+            >>> for state in simulation.iter_run(end_time=10, sampling_years=[0.1, 1, 5])
+            ...    print(state.mf_prevalence_in_population())
+            0.99
+            0.6
+            0.2
+
+        Args:
+            end_time (float): end time
+            sampling_years (list[float]): list of years to sample State
+
+        Yields:
+            Iterator[State]: Iterator of the simulation's state.
+        """
         ...
 
     def iter_run(
@@ -257,6 +331,9 @@ class Simulation:
             raise ValueError(
                 "You must provide sampling_interval, sampling_years or neither"
             )
+
+        if sampling_years:
+            sampling_years = sorted(sampling_years)
 
         sampling_years_idx = 0
         while self.state.current_time <= end_time:
