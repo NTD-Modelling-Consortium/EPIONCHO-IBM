24efdb8ad0398b3423ae8bf14f3ab1954ef6e15b
mark-todd <markpeter.todd@hotmail.co.uk>
2023-02-16 15:41:15 +0000

    Added multiple param sets to example

--- examples/test_one_hdf5_parallel.py
+++ examples/test_one_hdf5_parallel.py
@@ -9,24 +9,37 @@ benchmark_iters = 10
 
 new_file = h5py.File('test_one.hdf5', 'w')
 
-def run_sim(i):
-    params = Params(
-        treatment=TreatmentParams(start_time=0.1, interval_years=0.1, stop_time=130),
-        n_people=400,
-    )
+def run_sim(params):
     simulation = Simulation(start_time=0, params=params)
     simulation.run(end_time=1)
-    grp = new_file.create_group(f'iter_{str(i)}')
+    grp = new_file.create_group(f'iter_{str(params.seed)}')
     simulation.save(grp)
-    restored_grp = new_file[f'iter_{str(i)}']
-    assert isinstance(restored_grp, h5py.Group)
-    res_sim = Simulation.restore(restored_grp)
-    return res_sim.state.mf_prevalence_in_population()
+    # restored_grp = new_file[f'iter_{str(params.seed)}']
+    # assert isinstance(restored_grp, h5py.Group)
+    # res_sim = Simulation.restore(restored_grp)
+    return simulation.state.mf_prevalence_in_population()
 
+list_of_params = [
+    Params(
+        treatment=TreatmentParams(start_time=0.1, interval_years=0.1, stop_time=130),
+        n_people=400,
+        seed=2314
+    ),
+    Params(
+        treatment=TreatmentParams(start_time=0.1, interval_years=0.1, stop_time=130),
+        n_people=400,
+        seed=23144
+    ),
+    Params(
+        treatment=TreatmentParams(start_time=0.1, interval_years=0.1, stop_time=130),
+        n_people=400,
+        seed=23145
+    )
+]
 
 if __name__ == "__main__":
     list_of_stats: list[float] = process_map(
-        run_sim, range(benchmark_iters), max_workers=cpu_count()
+        run_sim, list_of_params, max_workers=cpu_count()
     )
 
     print(np.mean(list_of_stats))
