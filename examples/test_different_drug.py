dcd0eb1517a2320878fea597c0f2fd111bbec41b
mark-todd <markpeter.todd@hotmail.co.uk>
2023-02-23 17:39:57 +0000

    Added new different drug example

---
+++ examples/test_different_drug.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from epioncho_ibm import Params, Simulation, TreatmentParams, make_state_from_hdf5
+
+params = Params(
+    delta_time_days=2,
+    treatment=TreatmentParams(start_time=5, stop_time=130),
+    n_people=400,
+    seed=0,
+)
+
+simulation = Simulation(start_time=0, params=params, verbose=True)
+simulation.run(end_time=10.0)
+
+assert params.treatment is not None
+params.treatment.u_ivermectin = 0.01
+params.treatment.shape_parameter_ivermectin = 1.5
+params.treatment.lam_max = 30
+params.treatment.phi = 10
+params.treatment.permanent_infertility = 0.4
+
+simulation.reset_current_params(params)
+simulation.run(end_time=12.0)
+
+print(simulation.state.people.last_treatment)
