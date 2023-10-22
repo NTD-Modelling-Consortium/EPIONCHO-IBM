1124770e375c3d5776a127ba0d855b6119ce1fe9
tlestang <thibault.lestang@bdi.ox.ac.uk>
2023-10-22 12:36:30 +0000

    Inline calculation of coverage
    (whether someone is treated or not)

--- epioncho_ibm/advance/treatment.py
+++ epioncho_ibm/advance/treatment.py
@@ -7,35 +7,6 @@ from numpy.random import Generator
 from epioncho_ibm.state import Array, HumanParams, TreatmentParams
 
 
-def _calc_coverage(
-    ages: Array.Person.Float,
-    compliance: Array.Person.Bool,
-    measured_coverage: float,
-    age_of_compliance: float,
-    numpy_bit_gen: Generator,
-) -> Array.Person.Bool:
-    """
-    Calculates whether each person in the model is covered by a treatment.
-
-    Args:
-        ages (Array.Person.Float): The ages of the people in the model
-        compliance (Array.Person.Bool): Whether each person in the model is compliant
-        measured_coverage (float): A measured value of coverage assuming all people are compliant.
-        age_of_compliance (float): How old a person must be to be compliant
-        numpy_bit_gen: (Generator): The random number generator for numpy
-
-    Returns:
-        Array.Person.Bool: An array stating if each person in the model is treated
-    """
-    non_compliant_people = (ages < age_of_compliance) | ~compliance
-    compliant_percentage = 1 - np.mean(non_compliant_people)
-    coverage = measured_coverage / compliant_percentage
-    out_coverage = np.repeat(coverage, len(ages))
-    out_coverage[non_compliant_people] = 0
-    rand_nums = numpy_bit_gen.uniform(low=0, high=1, size=len(ages))
-    return rand_nums < out_coverage
-
-
 def _is_during_treatment(
     treatment: TreatmentParams,
     current_time: float,
@@ -93,7 +64,7 @@ def get_treatment(
     current_time: float,
     treatment_times: Optional[Array.Treatments.Float],
     ages: Array.Person.Float,
-    compliance: Optional[Array.Person.Bool],
+    compliance: Optional[Array.Person.Float],
     numpy_bit_gen: Generator,
 ) -> Optional[TreatmentGroup]:
     """
@@ -105,7 +76,7 @@ def get_treatment(
         current_time (float): The current time, t, in the model
         treatment_times (Optional[Array.Treatments.Float]): The times for treatment across the model.
         ages (Array.Person.Float): The ages of the people
-        compliance (Array.Person.Bool): The compliance of the people
+        compliance (Array.Person.Float): The compliance of the people
         numpy_bit_gen: (Generator): The random number generator for numpy
 
     Returns:
@@ -122,15 +93,13 @@ def get_treatment(
             treatment_times,
         )
         if treatment_started:
-            coverage_in = _calc_coverage(
-                ages,
-                compliance,
-                treatment_params.total_population_coverage,
-                treatment_params.min_age_of_treatment,
-                numpy_bit_gen,
-            )
+            rand_nums = numpy_bit_gen.uniform(low=0, high=1, size=len(ages))
+            too_young = ages < treatment_params.min_age_of_treatment
             return TreatmentGroup(
-                treatment_params, coverage_in, treatment_times, treatment_occurred
+                treatment_params=treatment_params
+                coverage_in=(rand_nums < compliance) & ~too_young,
+                treatment_times=treatment_times,
+                treatment_occurred=treatment_occurred,
             )
         else:
             return None
