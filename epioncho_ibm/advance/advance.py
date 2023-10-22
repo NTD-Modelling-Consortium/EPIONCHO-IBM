f8bb55f5610c44bdc20e2f6b80add2b10b2c05f2
tlestang <thibault.lestang@bdi.ox.ac.uk>
2023-10-22 12:27:44 +0000

    Initialise compliance for reset individuals
    based on current values for treatment correlation and coverage

--- epioncho_ibm/advance/advance.py
+++ epioncho_ibm/advance/advance.py
@@ -185,5 +185,9 @@ def advance_state(state: State, debug: bool = False) -> None:
         state.people.ages >= state._params.humans.max_human_age,
     )
     state.people.process_deaths(
-        people_to_die, state._params.humans.gender_ratio, state.numpy_bit_generator
+        people_to_die,
+        state._params.humans.gender_ratio,
+        state._params.treatment.correlation,
+        state._params.treatment.coverage,
+        state.numpy_bit_generator,
     )
